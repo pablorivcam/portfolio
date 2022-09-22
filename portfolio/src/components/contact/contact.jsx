c117454f2da26e50bb239c457b7195eb0f91a43c
pablo.rivas@globalincubator.com <pablo.rivas@globalincubator.com>
2022-09-22 12:41:07 +0200

    Added navbar

--- portfolio/src/components/contact/contact.jsx
+++ portfolio/src/components/contact/contact.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import './contact.css';
 import { FormattedMessage } from "react-intl";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export function Contact() {
     return <section id="contact">
         <div id="contact_container">
             <h1 className="section_title"><FormattedMessage id="contact_title" /></h1>
 
-            <p>2015 UDC </p>
-            <p>2015 UDC </p>
-            <p>Pablo Rivas Camino - 2022</p>
-            <p>Pablo Rivas Camino - 2022</p>
-            <p>Pablo Rivas Camino - 2022</p>
+            <p>
+                <FontAwesomeIcon className="fa-icon" icon={faEnvelope}></FontAwesomeIcon>
+                pablorivcam@gmail.com
+            </p>
+            <p>            <a href="https://www.linkedin.com/in/pablo-rivas-camino">LinkedIn</a>
+            </p>
+            <a href="https://github.com/pablorivcam">GitHub</a>
         </div>
     </section>
 }
--- portfolio/src/components/header/navbar/navbar.jsx
+++ portfolio/src/components/header/navbar/navbar.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { LOCALES } from "../../../i18n/locales";
 import './navbar.css';
+import { FormattedMessage } from "react-intl";
+import { Link } from "react-scroll";
 
 export const Navbar = (props) => {
+
+    const [isScrolled, setIsScrolled] = useState(false);
+    const [scroll, setScroll] = useState(0);
+    const [navbarHeight, setnavbarHeight] = useState(0);
 
     function changeLanguage(e) {
         props.handleChange(e.target.textContent);
@@ -20,12 +26,54 @@ export const Navbar = (props) => {
         return result;
     }
 
-    return <div id="navbar">
+    window.addEventListener('scroll', e => {
+        setScroll(window.pageYOffset);
+        setIsScrolled(window.pageYOffset > window.innerHeight - (document.getElementById("navbar").offsetHeight));
+        setnavbarHeight(document.getElementById("navbar").offsetHeight);
+    });
+
+    function isHighlighted(id) {
+        const element = document.getElementById(id);
+        if (element === null) {
+            return false;
+        }
+
+        const elementPosition = window.innerHeight + element.offsetTop - navbarHeight;
+
+        return (window.pageYOffset > elementPosition) && (window.pageYOffset < elementPosition + element.offsetHeight);
+    }
+
+    return <div id="navbar" className={window.pageYOffset > window.innerHeight - navbarHeight ? 'scrolled_navbar' : ''}>
         <div id="navbar_container">
             <div id="navbar_right_container">
-                <svg width="24px" height="24px" viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"><g id="ic-places-mountains">
-                        <path className="cls-1" d="M9,12.42l4.83-6.76a.2.2,0,0,1,.33,0L21.8,18.11a.2.2,0,0,1-.17.31H13" /><path className="cls-1" d="M5.84,12.65,2.2,18.11a.2.2,0,0,0,.17.31h7.79a.2.2,0,0,0,.15-.32L6.16,12.64A.2.2,0,0,0,5.84,12.65Z" /></g></svg>
+                <Link to="header" smooth={true} duration={1000}>
+                    <svg width="24px" height="24px" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"><g id="ic-places-mountains">
+                            <path className="cls-1" d="M9,12.42l4.83-6.76a.2.2,0,0,1,.33,0L21.8,18.11a.2.2,0,0,1-.17.31H13" /><path className="cls-1" d="M5.84,12.65,2.2,18.11a.2.2,0,0,0,.17.31h7.79a.2.2,0,0,0,.15-.32L6.16,12.64A.2.2,0,0,0,5.84,12.65Z" /></g></svg>
+                </Link>
+            </div>
+            <div id="navbar_middle_container">
+                <Link to="about" smooth={true} duration={1000} >
+                    <div className={isHighlighted("about") ? "my_button2 highlight_element" : "my_button2"}>
+                        <FormattedMessage id="about_title" />
+                    </div>
+                </Link>
+                <Link to="experience" smooth={true} duration={1000}>
+                    <div className={isHighlighted("experience") ? "my_button2 highlight_element" : "my_button2"}>
+                        <FormattedMessage id="experience_title" />
+                    </div>
+                </Link>
+                <div style={{ padding: '1vh 10vw' }}></div>
+                <Link to="skills" smooth={true} duration={1000}>
+                    <div className={isHighlighted("skills") ? "my_button2 highlight_element" : "my_button2"}>
+                        <FormattedMessage id="skills_title" />
+                    </div>
+                </Link>
+                <Link to="contact" smooth={true} duration={1000}>
+                    <div className={isHighlighted("contact") ? "my_button2 highlight_element" : "my_button2"}>
+                        <FormattedMessage id="contact_title" />
+                    </div>
+                </Link>
             </div>
             <div id="navbar_language_container">
                 {getLanguageButtons()}
